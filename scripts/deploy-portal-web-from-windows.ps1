289dc909cd9c237acb712b1ba63eb9cba860e1aa
ssrrrrrrrr <326758637@qq.com>
2026-05-29 17:14:14 +0800

    chore: add portal web deploy script

---
+++ scripts/deploy-portal-web-from-windows.ps1
@@ -0,0 +1,22 @@
+﻿param(
+  [string]$VmHost = "192.168.30.11",
+  [string]$VmUser = "root",
+  [string]$RepoDir = "/root/slo-rollout-demo"
+)
+
+$ErrorActionPreference = "Stop"
+
+Write-Output "===== build Portal frontend ====="
+Push-Location web
+pnpm run build
+Pop-Location
+
+Write-Output "===== upload dist to VM ====="
+ssh "$VmUser@$VmHost" "rm -rf $RepoDir/web/dist"
+scp -r web/dist "$VmUser@$VmHost`:$RepoDir/web/"
+
+Write-Output "===== restart Portal Web on VM ====="
+ssh "$VmUser@$VmHost" "cd $RepoDir && bash scripts/start-portal-web.sh && bash scripts/status-portal-web.sh"
+
+Write-Output "===== Portal Web ready ====="
+Write-Output "http://$VmHost`:18081"
